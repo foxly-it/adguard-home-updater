766c4b7c0301ecfe9e91eafe1e54270efa558019
Foxly <info@foxly.de>
2026-03-15 11:40:05 +0100

    feat: add GitHub Pages install portal

--- docs/install.sh
+++ docs/install.sh
@@ -2,5 +2,5 @@
 set -e
 
 curl -fsSL \
-https://raw.githubusercontent.com/foxly-it/adguard-home-updater/main/install.sh \
-| bash
+    https://raw.githubusercontent.com/foxly-it/adguard-home-updater/main/install.sh |
+    bash
